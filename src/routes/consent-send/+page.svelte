fcde5a271a3748ad159ad9433575b2820c8e3a2a
ghostiger1117 <ghostiger1117@gmail.com>
2025-08-05 12:34:31 +0300

    consent send and create child profile page.

---
+++ src/routes/consent-send/+page.svelte
@@ -0,0 +1,573 @@
+<div class="parental-consent-required">
+  <div class="frame-1410104014">
+      <div class="frame-1410104015">
+          <div class="button">
+              <div class="arrowleft">
+                  <div class="vector"></div>
+              </div>
+              <div><span class="backtohome_span">Back To Home</span></div>
+          </div>
+          <div class="pop-up">
+              <div class="logo-text-full">
+                  <div class="drawtopia"></div>
+                  <div class="subtract"></div>
+              </div>
+              <div class="stroke"></div>
+              <div class="heading">
+                  <div class="parental-consent-required_01"><span class="parentalconsentrequired_01_span">Parental Consent Required</span></div>
+                  <div class="we-need-your-parents-permission-to-create-your-drawtopia-account"><span class="weneedyourparentspermissiontocreateyourdrawtopiaaccount_span">We need your parent's permission to create your Drawtopia account</span></div>
+              </div>
+              <div class="notification">
+                  <div class="info">
+                      <div class="vector_01"></div>
+                  </div>
+                  <div class="your-safety-matters-drawtopia-is-committed-to-protecting-children-online-we-follow-strict-privacy-guidelines-and-never-share-personal-information"><span class="yoursafetymattersdrawtopiaiscommittedtoprotectingchildrenonlinewefollowstrictprivacyguidelinesandneversharepersonalinformation_span">Your Safety Matters: Drawtopia is committed to protecting children online. We follow strict privacy guidelines and never share personal information.</span></div>
+              </div>
+              <div class="frame-1410103993">
+                  <div class="how-we-keep-you-safe"><span class="howwekeepyousafe_span">How We Keep You Safe</span></div>
+                  <div class="information">
+                      <div class="information_01">
+                          <div class="secure-private"><span class="secureprivate_span">Secure & Private</span></div>
+                          <div class="all-your-stories-and-drawings-are-kept-private-and-secure"><span class="allyourstoriesanddrawingsarekeptprivateandsecure_span">All your stories and drawings are kept private and secure</span></div>
+                      </div>
+                      <div class="information_02">
+                          <div class="parent-control"><span class="parentcontrol_span">Parent Control</span></div>
+                          <div class="parents-can-see-all-activities-and-control-account-settings"><span class="parentscanseeallactivitiesandcontrolaccountsettings_span">Parents can see all activities and control account settings</span></div>
+                      </div>
+                      <div class="information_03">
+                          <div class="age-appropriate"><span class="age-appropriate_span">Age-Appropriate</span></div>
+                          <div class="content-is-always-suitable-for-your-age-and-development"><span class="contentisalwayssuitableforyourageanddevelopment_span">Content is always suitable for your age and development</span></div>
+                      </div>
+                  </div>
+              </div>
+              <div class="form">
+                  <div class="parents-email-address"><span class="parentsemailaddress_span">Parent's Email Address</span></div>
+                  <div class="input-placeholder">
+                      <div><span class="drawtopiaexamplecom_span">drawtopia@example.com</span></div>
+                  </div>
+                  <div class="well-send-a-consent-request-to-this-email-address"><span class="wellsendaconsentrequesttothisemailaddress_span">We'll send a consent request to this email address</span></div>
+              </div>
+              <div class="frame-1410103851">
+                  <div class="frame-1410103841">
+                      <div class="button_01">
+                          <div class="send-consent-request"><span class="sendconsentrequest_span">Send Consent Request</span></div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+  <div class="frame-1410103821">
+      <div class="contact-us-hellodrawtopiacom"><span class="contactushellodrawtopiacom_span">Contact us: hello@drawtopia.com</span></div>
+      <div class="rectangle-34"></div>
+      <div class="frame-1410103820">
+          <div class="privacy-policy"><span class="privacypolicy_span">Privacy Policy</span></div>
+          <div class="terms-of-service"><span class="termsofservice_span">Terms of Service</span></div>
+      </div>
+  </div>
+</div>
+
+<style>
+.vector {
+  width: 18px;
+  height: 15px;
+  left: 3px;
+  top: 4.50px;
+  position: absolute;
+  background: black;
+}
+
+.backtohome_span {
+  color: black;
+  font-size: 24px;
+  font-family: Quicksand;
+  font-weight: 600;
+  word-wrap: break-word;
+}
+
+.drawtopia {
+  width: 267.53px;
+  height: 50px;
+  left: 0px;
+  top: 0px;
+  position: absolute;
+  background: radial-gradient(ellipse 50.00% 50.00% at 50.00% 50.00%, #0FE3EF 44%, #438BFF 100%);
+  box-shadow: 0px 0.7471606731414795px 2.2414820194244385px #871FFF inset;
+}
+
+.subtract {
+  width: 37.36px;
+  height: 37.36px;
+  left: 159.94px;
+  top: 2.40px;
+  position: absolute;
+  transform: rotate(4deg);
+  transform-origin: top left;
+  background: radial-gradient(ellipse 50.00% 50.00% at 50.00% 50.00%, #0FE3EF 44%, #438BFF 100%);
+  box-shadow: 0px 0.7471607327461243px 2.2414822578430176px #871FFF inset;
+}
+
+.stroke {
+  align-self: stretch;
+  height: 1px;
+  background: #EDEDED;
+}
+
+.parentalconsentrequired_01_span {
+  color: #141414;
+  font-size: 32px;
+  font-family: Quicksand;
+  font-weight: 600;
+  line-height: 44.80px;
+  word-wrap: break-word;
+}
+
+.parental-consent-required_01 {
+  align-self: stretch;
+}
+
+.weneedyourparentspermissiontocreateyourdrawtopiaaccount_span {
+  color: #666D80;
+  font-size: 20px;
+  font-family: Nunito;
+  font-weight: 400;
+  line-height: 28px;
+  word-wrap: break-word;
+}
+
+.we-need-your-parents-permission-to-create-your-drawtopia-account {
+  align-self: stretch;
+}
+
+.vector_01 {
+  width: 26px;
+  height: 26px;
+  left: 3px;
+  top: 3px;
+  position: absolute;
+  background: #141414;
+  background-image: url("../../assets/Info.svg");
+}
+
+.yoursafetymattersdrawtopiaiscommittedtoprotectingchildrenonlinewefollowstrictprivacyguidelinesandneversharepersonalinformation_span {
+  color: #141414;
+  font-size: 18px;
+  font-family: Quicksand;
+  font-weight: 600;
+  line-height: 25.20px;
+  word-wrap: break-word;
+}
+
+.your-safety-matters-drawtopia-is-committed-to-protecting-children-online-we-follow-strict-privacy-guidelines-and-never-share-personal-information {
+  flex: 1 1 0;
+}
+
+.howwekeepyousafe_span {
+  color: black;
+  font-size: 16px;
+  font-family: Quicksand;
+  font-weight: 600;
+  line-height: 19.20px;
+  word-wrap: break-word;
+}
+
+.how-we-keep-you-safe {
+  align-self: stretch;
+}
+
+.secureprivate_span {
+  color: black;
+  font-size: 16px;
+  font-family: Quicksand;
+  font-weight: 600;
+  line-height: 19.20px;
+  word-wrap: break-word;
+}
+
+.secure-private {
+  align-self: stretch;
+}
+
+.allyourstoriesanddrawingsarekeptprivateandsecure_span {
+  color: black;
+  font-size: 14px;
+  font-family: Quicksand;
+  font-weight: 400;
+  line-height: 16.80px;
+  word-wrap: break-word;
+}
+
+.all-your-stories-and-drawings-are-kept-private-and-secure {
+  align-self: stretch;
+}
+
+.parentcontrol_span {
+  color: black;
+  font-size: 16px;
+  font-family: Quicksand;
+  font-weight: 600;
+  line-height: 19.20px;
+  word-wrap: break-word;
+}
+
+.parent-control {
+  align-self: stretch;
+}
+
+.parentscanseeallactivitiesandcontrolaccountsettings_span {
+  color: black;
+  font-size: 14px;
+  font-family: Quicksand;
+  font-weight: 400;
+  line-height: 16.80px;
+  word-wrap: break-word;
+}
+
+.parents-can-see-all-activities-and-control-account-settings {
+  align-self: stretch;
+}
+
+.age-appropriate_span {
+  color: black;
+  font-size: 16px;
+  font-family: Quicksand;
+  font-weight: 600;
+  line-height: 19.20px;
+  word-wrap: break-word;
+}
+
+.age-appropriate {
+  align-self: stretch;
+}
+
+.contentisalwayssuitableforyourageanddevelopment_span {
+  color: black;
+  font-size: 14px;
+  font-family: Quicksand;
+  font-weight: 400;
+  line-height: 16.80px;
+  word-wrap: break-word;
+}
+
+.content-is-always-suitable-for-your-age-and-development {
+  align-self: stretch;
+}
+
+.parentsemailaddress_span {
+  color: #141414;
+  font-size: 16px;
+  font-family: Quicksand;
+  font-weight: 600;
+  line-height: 22.40px;
+  word-wrap: break-word;
+}
+
+.parents-email-address {
+  align-self: stretch;
+}
+
+.drawtopiaexamplecom_span {
+  color: #141414;
+  font-size: 16px;
+  font-family: Nunito;
+  font-weight: 400;
+  line-height: 22.40px;
+  word-wrap: break-word;
+}
+
+.wellsendaconsentrequesttothisemailaddress_span {
+  color: #666D80;
+  font-size: 14px;
+  font-family: Quicksand;
+  font-weight: 400;
+  line-height: 19.60px;
+  word-wrap: break-word;
+}
+
+.well-send-a-consent-request-to-this-email-address {
+  align-self: stretch;
+}
+
+.sendconsentrequest_span {
+  color: white;
+  font-size: 18px;
+  font-family: Quicksand;
+  font-weight: 600;
+  line-height: 25.20px;
+  word-wrap: break-word;
+}
+
+.send-consent-request {
+  text-align: center;
+}
+
+.contactushellodrawtopiacom_span {
+  color: #141414;
+  font-size: 18px;
+  font-family: Nunito;
+  font-weight: 400;
+  line-height: 25.20px;
+  word-wrap: break-word;
+}
+
+.contact-us-hellodrawtopiacom {
+  text-align: center;
+}
+
+.rectangle-34 {
+  align-self: stretch;
+  height: 1px;
+  background: #EDEDED;
+}
+
+.privacypolicy_span {
+  color: #141414;
+  font-size: 18px;
+  font-family: Nunito;
+  font-weight: 400;
+  line-height: 25.20px;
+  word-wrap: break-word;
+}
+
+.privacy-policy {
+  text-align: center;
+}
+
+.termsofservice_span {
+  color: #141414;
+  font-size: 18px;
+  font-family: Nunito;
+  font-weight: 400;
+  line-height: 25.20px;
+  word-wrap: break-word;
+}
+
+.terms-of-service {
+  text-align: center;
+}
+
+.heading {
+  align-self: stretch;
+  padding-left: 4px;
+  padding-right: 4px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 8px;
+  display: flex;
+}
+
+.information_01 {
+  flex: 1 1 0;
+  padding: 12px;
+  background: #EEF6FF;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 8px;
+  display: inline-flex;
+}
+
+.information_02 {
+  flex: 1 1 0;
+  padding: 12px;
+  background: #ECFFFF;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 8px;
+  display: inline-flex;
+}
+
+.information_03 {
+  flex: 1 1 0;
+  padding: 12px;
+  background: #F5F2FF;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 8px;
+  display: inline-flex;
+}
+
+.input-placeholder {
+  align-self: stretch;
+  height: 50px;
+  padding-left: 10px;
+  padding-right: 10px;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  background: white;
+  overflow: hidden;
+  border-radius: 12px;
+  outline: 1px #DCDCDC solid;
+  outline-offset: -1px;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 10px;
+  display: inline-flex;
+}
+
+.button_01 {
+  align-self: stretch;
+  padding-left: 24px;
+  padding-right: 24px;
+  padding-top: 16px;
+  padding-bottom: 16px;
+  background: #438BFF;
+  border-radius: 20px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  display: inline-flex;
+}
+
+.frame-1410103820 {
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 24px;
+  display: inline-flex;
+}
+
+.arrowleft {
+  width: 24px;
+  height: 24px;
+  position: relative;
+  overflow: hidden;
+}
+
+.logo-text-full {
+  width: 267px;
+  height: 50px;
+  position: relative;
+}
+
+.info {
+  width: 32px;
+  height: 32px;
+  position: relative;
+  overflow: hidden;
+}
+
+.information {
+  align-self: stretch;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 8px;
+  display: inline-flex;
+}
+
+.form {
+  align-self: stretch;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 4px;
+  display: flex;
+}
+
+.frame-1410103841 {
+  align-self: stretch;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 8px;
+  display: flex;
+}
+
+.frame-1410103821 {
+  align-self: stretch;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+  display: flex;
+}
+
+.button {
+  justify-content: flex-start;
+  align-items: center;
+  gap: 22px;
+  display: inline-flex;
+}
+
+.notification {
+  align-self: stretch;
+  padding: 10px;
+  background: #F8FAFB;
+  border-radius: 12px;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 16px;
+  display: inline-flex;
+}
+
+.frame-1410103993 {
+  align-self: stretch;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 12px;
+  display: flex;
+}
+
+.frame-1410103851 {
+  align-self: stretch;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 16px;
+  display: flex;
+}
+
+.pop-up {
+  width: 686px;
+  padding-top: 24px;
+  padding-bottom: 16px;
+  padding-left: 12px;
+  padding-right: 12px;
+  background: white;
+  box-shadow: 0px 5px 4px rgba(0, 0, 0, 0.10);
+  border-radius: 20px;
+  outline: 1px #EDEDED solid;
+  outline-offset: -1px;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 24px;
+  display: flex;
+}
+
+.frame-1410104015 {
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 48px;
+  display: flex;
+}
+
+.frame-1410104014 {
+  align-self: stretch;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  display: flex;
+}
+
+.parental-consent-required {
+  width: 100%;
+  height: 100%;
+  padding-left: 48px;
+  padding-right: 48px;
+  padding-top: 80px;
+  padding-bottom: 80px;
+  background: white;
+  overflow: hidden;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 48px;
+  display: inline-flex;
+}
+</style>
